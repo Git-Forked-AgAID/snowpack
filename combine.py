04ec5e2046b4c53719f5c1ad19bab3ec3392dd79
Cole Wilson <colewilson.main@gmail.com>
2025-01-31 22:51:08 -0800

    remove .DS_Store

--- combine.py
+++ combine.py
@@ -5,8 +5,9 @@ snotel_df = pd.read_csv('./processed/out.csv')
 met_df = pd.read_csv('./processed/met_out.csv')
 met_df = met_df.loc[met_df["date"]=="1990-01-01"]
 met_gdf = gpd.GeoDataFrame(met_df, geometry=gpd.points_from_xy(met_df.lat, met_df.lon))
-met_gdf.explore(height=300,width=400)
-
+exp = met_gdf.explore(height=300,width=400)
+outfp = r"processed/explore.html"
+exp.save(outfp)
 
 snotel_gdf = gpd.GeoDataFrame(snotel_df, geometry=gpd.points_from_xy(snotel_df.lat, snotel_df.lon))
 print("loaded")
